2858f1b972b852c507a4111429b2ab7d0700232b
deden <dedenr2004@gmail.com>
2024-11-05 12:52:25 +0700

    update ini lagi

--- lib/screens/landingPage.dart
+++ lib/screens/landingPage.dart
@@ -8,6 +8,20 @@ class landingPage extends StatefulWidget {
 }
 
 class _landingPageState extends State<landingPage> {
+
+  int curentIndex = 0;
+  Widget dotIndicator(int index){
+    return AnimatedContainer(duration: Duration(microseconds: 400),
+    margin: EdgeInsets.only(right: 6),
+    width: 20,
+    height: 4,
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),
+    color: index == curentIndex ? Colors.white : Colors.white54
+    ),
+    );
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
